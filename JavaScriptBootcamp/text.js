8e5563dba1cc813ff0ccde8fdaf3269ea0547802
Abishek-GH <abishek0149@gmail.com>
2024-10-02 23:44:31 +0530

    Load the Files

---
+++ JavaScriptBootcamp/text.js
@@ -0,0 +1,13 @@
+/* 
+JavaScript is a high-level object oriented, mult-paradigm programming language.
+@High-Level -> We don't have to worry about complex stuff like memory management
+@Multi-Paradigm Language -> We can use different styles of programming Like imperative or declrative programming. 
+@Imperative or Declrative programming -> Different wasy fo structuring the code.
+@Object-Oriented -> Based on objects, for storing most kinds of data
+@Programming Language -> Basically a tool which instructs the computer to do things
+
+ES6/ES2015 is the biggest JavaScript release ever.
+@ES -> EcmaScript 
+Modern JavaScript -> All the versions after ES6
+*/
+
